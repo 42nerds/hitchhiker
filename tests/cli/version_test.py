183a03ec14c2b858e5a72c11f4f7dfc740c177ee
MrOsdev <clemensbu@42nerds.com>
2023-08-23 18:11:53 +0200

    refactor: version_test

--- tests/cli/version_test.py
+++ tests/cli/version_test.py
@@ -1,96 +1,164 @@
 from tests.git_fixtures import *
 from click.testing import CliRunner
+import hitchhiker_module_control.version.semver as semver
 from hitchhiker_module_control.cli import main
 
-# FIXME: this is a horrible mess
+
+# versions: [(project, version)]
+def invoke_cli_version_cmd(repo, main_version, versions, prerelease=False):
+    workdir = repo.working_tree_dir
+
+    args = ["--workdir", workdir, "version"]
+    if prerelease:
+        args.append("--prerelease")
+    result = CliRunner().invoke(main, args)
+    assert result.exit_code == 0
+    # TODO: check output here
+
+    expected_output = f"main version: {str(semver.Version().parse(main_version))}\n"
+    for project, version in versions:
+        expected_output += (
+            f"project: {project} version: {str(semver.Version().parse(version))}\n"
+        )
+    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
+    assert result.output == expected_output
+    assert result.exit_code == 0
+
 
 def test_version_repo_empty(repo_empty):
     """test for Version"""
-    workdir = repo_empty.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.0\nproject: project1 version: 0.0.0\n"
+    repo = repo_empty
+    invoke_cli_version_cmd(repo, "0.0.0", [("project1", "0.0.0")])
+
 
 def test_version_repo_one_fix(repo_one_fix):
     """test for Version"""
-    workdir = repo_one_fix.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.1\nproject: project1 version: 0.0.1\n"
+    repo = repo_one_fix
+    invoke_cli_version_cmd(repo, "0.0.1", [("project1", "0.0.1")])
+
 
 def test_version_repo_multi_one_breaking_change(repo_multi_one_breaking_change):
     """test for Version"""
-    workdir = repo_multi_one_breaking_change.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 1.0.0\nproject: project1 version: 0.0.0\nproject: project2 version: 1.0.0\n"
+    repo = repo_multi_one_breaking_change
+    invoke_cli_version_cmd(
+        repo, "1.0.0", [("project1", "0.0.0"), ("project2", "1.0.0")]
+    )
+
 
 def test_version_repo_multi_project_commits(repo_multi_project_commits):
     """test for Version"""
-    workdir = repo_multi_project_commits.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 1.0.0\nproject: project1 version: 0.0.1\nproject: project2 version: 1.0.0\nproject: 1another_project version: 0.1.0\nproject: 2another_project version: 1.0.0\n"
+    repo = repo_multi_project_commits
+    invoke_cli_version_cmd(
+        repo,
+        "1.0.0",
+        [
+            ("project1", "0.0.1"),
+            ("project2", "1.0.0"),
+            ("1another_project", "0.1.0"),
+            ("2another_project", "1.0.0"),
+        ],
+    )
 
-def test_version_repo_multi_project_commits_before_tag(repo_multi_project_commits_before_tag):
+
+def test_version_repo_multi_project_commits_before_tag(
+    repo_multi_project_commits_before_tag,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_tag.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.0\nproject: project1 version: 0.0.0\nproject: project2 version: 0.0.0\nproject: 1another_project version: 0.0.0\nproject: 2another_project version: 0.0.0\n"
+    repo = repo_multi_project_commits_before_tag
+    invoke_cli_version_cmd(
+        repo,
+        "0.0.0",
+        [
+            ("project1", "0.0.0"),
+            ("project2", "0.0.0"),
+            ("1another_project", "0.0.0"),
+            ("2another_project", "0.0.0"),
+        ],
+    )
 
-def test_version_repo_multi_project_commits_before_tag_fix_after(repo_multi_project_commits_before_tag_fix_after):
+
+def test_version_repo_multi_project_commits_before_tag_fix_after(
+    repo_multi_project_commits_before_tag_fix_after,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_tag_fix_after.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.1\nproject: project1 version: 0.0.1\nproject: project2 version: 0.0.1\nproject: 1another_project version: 0.0.0\nproject: 2another_project version: 0.0.0\n"
+    repo = repo_multi_project_commits_before_tag_fix_after
+    invoke_cli_version_cmd(
+        repo,
+        "0.0.1",
+        [
+            ("project1", "0.0.1"),
+            ("project2", "0.0.1"),
+            ("1another_project", "0.0.0"),
+            ("2another_project", "0.0.0"),
+        ],
+    )
 
-def test_version_repo_multi_project_commits_before_prerelease_tag1(repo_multi_project_commits_before_prerelease_tag):
+
+def test_version_repo_multi_project_commits_before_prerelease_tag1(
+    repo_multi_project_commits_before_prerelease_tag,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_prerelease_tag.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 1.0.0\nproject: project1 version: 0.0.1\nproject: project2 version: 1.0.0\nproject: 1another_project version: 0.1.0\nproject: 2another_project version: 1.0.0\n"
+    repo = repo_multi_project_commits_before_prerelease_tag
+    invoke_cli_version_cmd(
+        repo,
+        "1.0.0",
+        [
+            ("project1", "0.0.1"),
+            ("project2", "1.0.0"),
+            ("1another_project", "0.1.0"),
+            ("2another_project", "1.0.0"),
+        ],
+    )
 
-def test_version_repo_multi_project_commits_before_prerelease_tag2(repo_multi_project_commits_before_prerelease_tag):
+
+def test_version_repo_multi_project_commits_before_prerelease_tag2(
+    repo_multi_project_commits_before_prerelease_tag,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_prerelease_tag.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--prerelease"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.0\nproject: project1 version: 0.0.0\nproject: project2 version: 0.0.0\nproject: 1another_project version: 0.0.0\nproject: 2another_project version: 0.0.0\n"
+    repo = repo_multi_project_commits_before_prerelease_tag
+    invoke_cli_version_cmd(
+        repo,
+        "0.0.0",
+        [
+            ("project1", "0.0.0"),
+            ("project2", "0.0.0"),
+            ("1another_project", "0.0.0"),
+            ("2another_project", "0.0.0"),
+        ],
+        prerelease=True,
+    )
 
 
-def test_version_repo_multi_project_commits_before_prerelease_tag_fix_after1(repo_multi_project_commits_before_prerelease_tag_fix_after):
+def test_version_repo_multi_project_commits_before_prerelease_tag_fix_after1(
+    repo_multi_project_commits_before_prerelease_tag_fix_after,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_prerelease_tag_fix_after.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 1.0.0\nproject: project1 version: 0.0.1\nproject: project2 version: 1.0.0\nproject: 1another_project version: 0.1.0\nproject: 2another_project version: 1.0.0\n"
+    repo = repo_multi_project_commits_before_prerelease_tag_fix_after
+    invoke_cli_version_cmd(
+        repo,
+        "1.0.0",
+        [
+            ("project1", "0.0.1"),
+            ("project2", "1.0.0"),
+            ("1another_project", "0.1.0"),
+            ("2another_project", "1.0.0"),
+        ],
+    )
 
-def test_version_repo_multi_project_commits_before_prerelease_tag_fix_after2(repo_multi_project_commits_before_prerelease_tag_fix_after):
+
+def test_version_repo_multi_project_commits_before_prerelease_tag_fix_after2(
+    repo_multi_project_commits_before_prerelease_tag_fix_after,
+):
     """test for Version"""
-    workdir = repo_multi_project_commits_before_prerelease_tag_fix_after.working_tree_dir
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--prerelease"])
-    assert result.exit_code == 0
-    result = CliRunner().invoke(main, ["--workdir", workdir, "version", "--show"])
-    assert result.exit_code == 0
-    assert result.output == "main version: 0.0.1-rc.0\nproject: project1 version: 0.0.0\nproject: project2 version: 0.0.1-rc.0\nproject: 1another_project version: 0.0.1-rc.0\nproject: 2another_project version: 0.0.0\n"
+    repo = repo_multi_project_commits_before_prerelease_tag_fix_after
+    invoke_cli_version_cmd(
+        repo,
+        "0.0.1-rc.0",
+        [
+            ("project1", "0.0.0"),
+            ("project2", "0.0.1-rc.0"),
+            ("1another_project", "0.0.1-rc.0"),
+            ("2another_project", "0.0.0"),
+        ],
+        prerelease=True,
+    )
