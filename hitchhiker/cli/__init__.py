8a55e8d076ee9731ab3d8265e088e9d5e2698281
MrOsdev <MrOsdev@users.noreply.github.com>
2023-08-29 12:11:42 +0200

    refactor: add missing __init__.py

--- hitchhiker/cli/__init__.py
+++
@@ -1,6 +0,0 @@
-# from hitchhiker.cli.commands.main import main
-# from hitchhiker.cli.commands.version import version
-# from hitchhiker.cli.commands.odoo import odoo
-
-# main.add_command(version)
-# main.add_command(odoo)
