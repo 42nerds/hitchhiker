d6b5a6185c3695533fb5183676dd2bbf7732e9bf
MrOsdev <MrOsdev@users.noreply.github.com>
2023-09-04 10:02:12 +0200

    feat(release): option to specify config file path

--- hitchhiker/cli/release/commands.py
+++ hitchhiker/cli/release/commands.py
@@ -2,22 +2,23 @@ import os
 import git
 import click
 import tomlkit
-import hitchhiker.cli.release.config as config
+import hitchhiker.cli.release.config as rconfig
 import hitchhiker.cli.release.version as version
 
 @click.group()
+@click.option("--config", default="pyproject.toml", help="configuration file path (relative to working directory)")
 @click.option("--workdir", default="./", help="working directory")
 @click.pass_context
-def release(ctx: click.Context, workdir):
+def release(ctx: click.Context, workdir, config):
     ctx.ensure_object(dict)
     try:
         repo = git.Repo(workdir)
     except (git.InvalidGitRepositoryError, git.NoSuchPathError):
         raise click.ClickException(message="Could not find git repository")
     try:
-        ctx.obj["RELEASE_CONF"] = config.create_context_from_raw_config(os.path.join(repo.working_tree_dir, "pyproject.toml"), repo)
+        ctx.obj["RELEASE_CONF"] = rconfig.create_context_from_raw_config(os.path.join(repo.working_tree_dir, config), repo)
     except (FileNotFoundError, tomlkit.exceptions.NonExistentKey) as e:
-        raise click.FileError("pyproject.toml", hint=str(e))
+        raise click.FileError(os.path.join(repo.working_tree_dir, config), hint=str(e))
 
 
 release.add_command(version.version)
