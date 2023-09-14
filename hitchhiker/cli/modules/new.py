313457a4e5946b3982857d0c9c8a12ed831b20cb
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2023-09-14 11:09:11 +0000

    feat(modules): modules new - run copier

--- hitchhiker/cli/modules/new.py
+++ hitchhiker/cli/modules/new.py
@@ -4,7 +4,10 @@ import copier
 
 @click.command(name="new", short_help="Create new Odoo module from copier template")
 @click.argument("name")
+@click.option("--template", is_flag=False, default="https://github.com/42nerds/copier_odoo_module.git", help="Copier template URL")
 @click.pass_context
-def new_cmd(ctx: click.Context, name):
+def new_cmd(ctx: click.Context, name, template):
     """Create new Odoo module from copier template"""
+    click.echo(f"Creating {name} from {template}")
+    copier.run_copy(template, name)
 
