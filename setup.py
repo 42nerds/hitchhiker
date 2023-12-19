8d8866888c64a26679ce5b9c54273dbe5d961ff2
MrOsdev <MrOsdev@users.noreply.github.com>
2023-12-19 10:57:28 +0100

    fix: typed marker in package data

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=["tests"]),
+    package_data={
+        "hitchhiker": ["py.typed"],
+    },
     include_package_data=True,
 )
