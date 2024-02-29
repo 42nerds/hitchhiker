848471032b22883b7034dd62a645287f640582aa
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-02-29 13:06:59 +0000

    fix: tests

--- hitchhiker/cli/modules/commands.py
+++ hitchhiker/cli/modules/commands.py
@@ -2,7 +2,6 @@ import click
 
 import hitchhiker.cli.modules.generate_addons_path as generate_addons_path_mod
 import hitchhiker.cli.modules.list as list_mod
-import hitchhiker.cli.modules.repo as repo_mod
 from hitchhiker.cli.modules.graph import graph_cmd
 
 # FIXME: all these commands need tests
@@ -20,7 +19,16 @@ def modules(ctx: click.Context) -> None:
 modules.add_command(graph_cmd)
 modules.add_command(list_mod.list_cmd)
 modules.add_command(generate_addons_path_mod.generate_addons_path_cmd)
-modules.add_command(repo_mod.repo_group)
+
+
+try:
+    import hitchhiker.cli.modules.repo as repo_mod
+
+    modules.add_command(repo_mod.repo_group)
+except ImportError:
+    pass
+    # FIXME: do these errors differently
+    # click.secho(f"Please install {e.name} for full functionality.", err=True, fg="red")
 
 
 try:
