c498a90d3573e6d968c3846e787e5d8d1384cf15
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-03-01 11:55:18 +0000

    feat: odoo backup command

---
+++ hitchhiker/cli/odoo/backup.py
@@ -0,0 +1,61 @@
+import json
+import os
+import subprocess
+import tempfile
+from typing import Any
+
+import click
+import click_odoo  # type: ignore[import, import-untyped]
+from click_odoo import odoo
+
+from hitchhiker.tools import backup
+
+
+def __backup_filestore(b: backup.GenericBackup, dbname: str) -> None:
+    path = odoo.tools.config.filestore(dbname)
+    if os.path.isdir(path):
+        b.add_dir(path, "filestore")
+
+
+def __backup_database(b: backup.GenericBackup, dbname: str) -> None:
+    cmd = [odoo.tools.find_pg_tool("pg_dump"), "--no-owner", dbname]
+    env = odoo.tools.exec_pg_environ()
+    with tempfile.NamedTemporaryFile("w") as f:
+        cmd.insert(-1, "--file=" + f.name)
+        subprocess.run(
+            cmd,
+            env=env,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+            check=True,
+        )
+        f.flush()
+        os.fsync(f.fileno())
+        b.add_file(f.name, "dump.sql")
+
+
+def __dump_manifest(b: backup.GenericBackup, dbname: str) -> None:
+    with tempfile.NamedTemporaryFile("w") as f:
+        db = odoo.sql_db.db_connect(dbname)
+        with db.cursor() as cr:
+            json.dump(odoo.service.db.dump_db_manifest(cr), f, indent=4)
+        f.flush()
+        os.fsync(f.fileno())
+        b.add_file(f.name, "manifest.json")
+
+
+@click.command("backup")
+@click_odoo.env_options(default_log_level="error")  # type: ignore[misc]
+@click.option("-o", "--output", required=True, help="output zip file path")
+def backup_cmd(env: Any, output: str) -> None:  # pylint: disable=unused-argument
+    """Backup Odoo"""
+    dbname = odoo.tools.config["db_name"]
+    click.echo(f"starting backup on database: {dbname}")
+    with backup.backup(output, "zip") as b:
+        click.echo("backing up filestore")
+        __backup_filestore(b, dbname)
+        click.echo("backing up database")
+        __backup_database(b, dbname)
+        click.echo("creating manifest")
+        __dump_manifest(b, dbname)
+    click.echo(f"created backup: {output}")
--- hitchhiker/cli/odoo/commands.py
+++ hitchhiker/cli/odoo/commands.py
@@ -2,31 +2,13 @@ import click
 
 from hitchhiker.cli.odoo.dependency_graph import dependency_graph
 
+from . import backup
+
 
 @click.group()
-@click.option(
-    "-c",
-    "--config",
-    default="./odoo.conf",
-    help="path to odoo configuration file",
-    type=click.Path(exists=True),
-)
-@click.pass_context
-def odoo(ctx: click.Context, config: str) -> None:
+def odoo() -> None:
     """Odoo related commands"""
-    ctx.obj["ODOO_CONF"] = config
-
-
-@odoo.command()
-@click.pass_context
-def backup(_ctx: click.Context) -> None:
-    """Backup Odoo"""
-
-
-@odoo.command()
-@click.pass_context
-def staging(_ctx: click.Context) -> None:
-    """Backup Odoo"""
 
 
 odoo.add_command(dependency_graph)
+odoo.add_command(backup.backup_cmd)
