4675b372e84e496c108b957403576151856fc3f0
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-03-04 11:13:32 +0000

    fix: added setup.py for backwards compat with old pip

--- setup.py
+++ setup.py
@@ -2,11 +2,44 @@ from setuptools import setup
 
 _hitchhiker_version = "0.0.0"
 
-# for backwards compatibility
+# this is for backwards compatibility
+# NOTE: if you edit dependencies here edit them in pyproject.toml too!
 setup(
     name="hitchhiker",
     version=_hitchhiker_version,
-    packages="hitchhiker",
+    description="",
+    python_requires=">=3.7",
+    install_requires=[
+        "click>=8,<9",
+        "tomlkit>=0.12.3,<1",
+        "dotty-dict>=1.3.1,<2",
+        "typing-extensions",
+        "requests",
+        "jinja2",
+    ],
+    extras_require={
+        "release": ["gitpython>=3.1.40,<4", "PyGithub>=2.1.1,<3"],
+        "odoo": ["click-odoo>=1.6,<2"],
+        "test": [
+            "pytest>=7,<8",
+            "mypy",
+            "types-requests",
+            "pylint",
+            "flake8",
+        ],
+        "docs": [
+            "pdoc",
+        ],
+        "copier": [
+            "copier>=8.0.0,<10",
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "hitchhiker = hitchhiker.cli:cli.cli",
+        ]
+    },
+    packages=["hitchhiker"],
     package_data={
         "hitchhiker": ["py.typed"],
     },
