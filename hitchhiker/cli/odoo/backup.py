369a820b2086fdacd0e6c40942b33f6ba63a18f3
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-03-08 15:27:11 +0000

    fix: added a warning when filestore not found

--- hitchhiker/cli/odoo/backup.py
+++ hitchhiker/cli/odoo/backup.py
@@ -14,8 +14,10 @@ from . import click_odoo_ext
 
 def __backup_filestore(b: backup.GenericBackup, dbname: str) -> None:
     path = odoo.tools.config.filestore(dbname)
-    if os.path.isdir(path):
-        b.add_dir(path, "filestore")
+    if not os.path.isdir(path):
+        click.echo(f"WARNING: could not find filestore at {path}")
+        return
+    b.add_dir(path, "filestore")
 
 
 def __backup_database(b: backup.GenericBackup, dbname: str) -> None:
