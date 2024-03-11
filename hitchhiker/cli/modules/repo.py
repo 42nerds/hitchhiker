31a178d7b4d40cfe9dba92848086e9ec6fa9ae2e
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-03-11 14:19:49 +0000

    refactor: modules repo

--- hitchhiker/cli/modules/repo.py
+++ hitchhiker/cli/modules/repo.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 import os.path
 import re
@@ -25,7 +27,6 @@ class IndentDumper(yaml.Dumper):  # type: ignore[misc]
 
 # pylint: disable=R0801
 def _get_github_token(ctx: click.Context) -> str:
-    print(os.environ)
     if "GITHUB_TOKEN" not in os.environ and not ctx.obj["CONF"].has_key("GITHUB_TOKEN"):
         raise click.UsageError(
             "GitHub token not found, set the GITHUB_TOKEN environment variable or config option"
@@ -40,48 +41,95 @@ def _get_github_token(ctx: click.Context) -> str:
     )
 
 
-def _get_repo_mod_versions(
-    ctx: click.Context, repo: str, odoo_version: int
-) -> dict[str, tuple[semver.Version, str]]:
+_do_cached_download_cache: dict[str, str] = {}
+
+
+def _do_cached_download(ctx: click.Context, url: str) -> str:
     """
-    Retrieves the latest version from a module repo
+    Returns:
+        str: file path
+    """
+    if url not in _do_cached_download_cache:
+        san_url = "".join(i for i in url if i not in "\\/:*?<>|")
+        fname = f"tarball_{san_url}.tar.gz"
+        req = requests.get(
+            url,
+            allow_redirects=True,
+            headers={"Authorization": f"Bearer {_get_github_token(ctx)}"},
+            timeout=30,
+        )
+        with open(fname, "wb") as f:  # type: ignore[assignment]
+            f.write(req.content)  # type: ignore[arg-type]
+        _do_cached_download_cache[url] = fname
+    return _do_cached_download_cache[url]
+
+
+def _clear_download_cache() -> None:
+    """
+    Deletes files downloaded with _do_cached_download
+    """
+    global _do_cached_download_cache  # pylint: disable=global-statement
+    for fname in _do_cached_download_cache.values():
+        os.remove(fname)
+    _do_cached_download_cache = {}
+
+
+def _tag_as_version(tag: str, odoo_version: int) -> semver.Version:
+    tag_regex = rf"^{odoo_version}\.0-v?(\d+\.\d+\.\d+)$"
+    return semver.Version().parse(
+        re.match(tag_regex, tag).group(1)  # type: ignore[union-attr]
+    )
+
+
+_get_repo_latest_tag_cache: dict[str, tuple[str, str]] = {}
+
+
+def _get_repo_latest_tag(
+    ctx: click.Context, repo: str, odoo_version: int
+) -> tuple[str, str]:
+    """
+    Retrieves the latest tag from a module repo
 
     Parameters:
         ctx (click.Context): The Click context object.
 
     Returns:
-        dict[str, tuple[semver.Version, str]]: dict[modulename, tuple[version, tgzurl]].
+        tuple[str, str]: tuple[tagname, tgzurl].
 
     """
+    cache_key = repo + str(odoo_version)
+    if cache_key in _get_repo_latest_tag_cache:
+        return _get_repo_latest_tag_cache[cache_key]
+
     try:
         gh = github.Github(_get_github_token(ctx))
-        releases = gh.get_repo(repo).get_releases()
+        tags = gh.get_repo(repo).get_tags()
     except Exception as e:
         click.secho(
-            "Error getting releases from GitHub. Is something wrong with your token?",
+            "Error getting tags from GitHub. Is something wrong with your token?",
             err=True,
             fg="red",
         )
         raise e
-    ret = {}
-    release_name_regex = rf"^{odoo_version}\.0-v?(\d+\.\d+\.\d+)$"
-    release = sorted(
-        list(filter(lambda r: re.match(release_name_regex, r.title), releases)),
-        key=lambda r: semver.Version().parse(
-            re.match(release_name_regex, r.title).group(1)  # type: ignore[union-attr]
-        ),
-    )[0]
-    module_list_ent_regex = (
-        r"^\| (?P<name>\w+) (?::boom: )?\| (?P<version>\d+\.\d+\.\d+) (?::new: )?\|$"
+    tag_regex = rf"^{odoo_version}\.0-v?(\d+\.\d+\.\d+)$"
+    tags_sorted = sorted(
+        list(filter(lambda r: re.match(tag_regex, r.name), tags)),
+        key=lambda r: _tag_as_version(r.name, odoo_version),
+        reverse=True,
     )
-    for line in release.body.splitlines():
-        match = re.match(module_list_ent_regex, line)
-        if match is not None:
-            ret[match.group("name")] = (
-                semver.Version().parse(match.group("version")),
-                release.tarball_url,
-            )
-    return ret
+    if len(tags_sorted) == 0:
+        errstr = f"No tags found in repo {repo} for odoo version {odoo_version}"
+        click.secho(
+            errstr,
+            err=True,
+            fg="red",
+        )
+        raise RuntimeError(errstr)
+
+    tag = tags_sorted[0]
+
+    _get_repo_latest_tag_cache[cache_key] = (tag.name, tag.tarball_url)
+    return _get_repo_latest_tag_cache[cache_key]
 
 
 @click.group("repo")
@@ -130,9 +178,6 @@ def create_cmd(_ctx: click.Context, glob: str, overwrite: bool) -> None:
             path = str(os.path.dirname(module.get_dir()))
             if path != ".":
                 mod["path"] = path
-            version = module.get_version()
-            if version is not None:
-                mod["version"] = str(version)
             mods.append(mod)
         f.write(
             """# Example:
@@ -140,23 +185,22 @@ def create_cmd(_ctx: click.Context, glob: str, overwrite: bool) -> None:
 #modules:
 #  - name: test_abc
 #    path: ./test_modules
-#    version: 0.1.0
 #    git: 42nerds/module_test_inroot
+#    git_tag: 17.0-v0.2.0
 #    git_path: . # by default git path will be the same as the module name
 #  - name: test_abcd
 #    path: ./test_modules
-#    use_latest: true # if true will always clone the latest version. False by default
-#    version: 0.3.0
+#    use_latest: true # if true will always clone the latest version update it in the vogon.yaml. False by default
 #    git: 42nerds/module_test
+#    git_tag: 17.0-v0.0.1
 #  - name: another_module
-#    version: 0.3.0
 #    git: 42nerds/module_test
+#    git_tag: 17.0-v0.1.0
 #  - name: module_without_git
-#    version: 0.0.5
 # Autogenerated from modules found in the current working directory:
 """
         )
-        f.write("odoo_version: 0 # set this\n")
+        f.write("odoo_version: 0 # set this!!!\n")
         f.write(
             yaml.dump(
                 {"modules": mods},
@@ -169,6 +213,26 @@ def create_cmd(_ctx: click.Context, glob: str, overwrite: bool) -> None:
 
 
 repo_group.add_command(create_cmd)
+
+
+def _extract_tarball(ctx: click.Context, url: str, git_path: str, tgt_path: str) -> None:
+    # extract the tar file to a temporary directory, then copy what we need and delete it again
+    with tarfile.open(_do_cached_download(ctx, url)) as tar:
+        # https://stackoverflow.com/a/43094365
+        def remove_top_dir(tf: Any) -> Any:
+            top = (
+                os.path.commonprefix(tf.getnames()) + "/"
+            )  # NOTE: works ONLY with module repos
+            for member in tf.getmembers():
+                if member.path.startswith(top):
+                    member.path = member.path[len(top):]
+                    yield member
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            tar.extractall(path=tmpdir, members=remove_top_dir(tar))
+            if os.path.isdir(tgt_path):
+                shutil.rmtree(tgt_path)
+            shutil.copytree(tmpdir + "/" + git_path, tgt_path)
 
 
 # prospector: disable=MC0001
@@ -204,69 +268,55 @@ def update_cmd(ctx: click.Context, module: str) -> None:
         except StopIteration:
             click.echo(f"module {module} not found")
             return
-    git_mods = {}  # keeps track of github release requests
-    git_repos_downloaded = {}  # keeps track of tarballs we downloaded
     conf["modules"] = []  # we add the modules back into the list after we made changes.
     for mod in to_update:
+        click.echo(f"processing {mod['name']}")
         repo = mod.get("git")
         if repo is None:  # no git repo set, continuing is pointless
+            click.echo("    skipping.. No repo set")
             conf["modules"].append(mod)
             continue
-        # get the release info if we didn't already
-        if repo not in git_mods:
-            git_mods[repo] = _get_repo_mod_versions(ctx, repo, conf["odoo_version"])
-        if mod["name"] not in git_mods[repo]:
-            click.echo(f"module {mod['name']} not found in repo {repo}")
-            return
-        # is there a newer version available?
-        if not mod.get("use_latest") and not (
-            git_mods[repo][mod["name"]][0] > semver.Version().parse(mod["version"])
-        ):
-            click.echo(f"no newer version for {mod['name']} in repo {repo}")
-            conf["modules"].append(mod)
-            continue
-        click.echo(
-            f"updating {mod['name']} to {str(git_mods[repo][mod['name']][0])} from { mod['version']}"
-        )
-        mod["version"] = str(
-            git_mods[repo][mod["name"]][0]
-        )  # set the version to our new version
-        conf["modules"].append(mod)
-        # get the tarball if we didn't already
-        if repo not in git_repos_downloaded:
-            fname = f"release_{repo.replace('/', '_')}.tar.gz"
-            req = requests.get(
-                git_mods[repo][mod["name"]][1],
-                allow_redirects=True,
-                headers={"Authorization": f"Bearer {_get_github_token(ctx)}"},
-                timeout=30,
-            )
-            with open(fname, "wb") as f:  # type: ignore[assignment]
-                f.write(req.content)  # type: ignore[arg-type]
-            git_repos_downloaded[repo] = fname
+
         # initialize our paths
         mod_path = mod.get("path", ".") + "/" + mod["name"]
         git_path = mod.get("git_path", f"./{mod['name']}")
-        # extract the tar file to a temporary directory, then copy what we need and delete it again
-        with tarfile.open(git_repos_downloaded[repo]) as tar:
-            # https://stackoverflow.com/a/43094365
-            def remove_top_dir(tf: Any) -> Any:
-                top = (
-                    os.path.commonprefix(tf.getnames()) + "/"
-                )  # NOTE: works ONLY with module repos
-                for member in tf.getmembers():
-                    if member.path.startswith(top):
-                        member.path = member.path[len(top):]
-                        yield member
 
-            with tempfile.TemporaryDirectory() as tmpdir:
-                tar.extractall(path=tmpdir, members=remove_top_dir(tar))
-                if os.path.isdir(mod_path):
-                    shutil.rmtree(mod_path)
-                shutil.copytree(tmpdir + "/" + git_path, mod_path)
-    # delete the tarballs we donwloaded
-    for fname in git_repos_downloaded.values():
-        os.remove(fname)
+        # do we not want to upgrade the module?
+        if not mod.get("use_latest"):
+            # does the module not exist yet?
+            if not os.path.isdir(mod_path):
+                click.echo("    performing initial download")
+                _extract_tarball(
+                    ctx,
+                    f"https://api.github.com/repos/{repo}/tarball/{mod['tag']}",
+                    git_path,
+                    mod_path,
+                )
+            else:
+                click.echo("    skipping.. Already exists and no use_latest")
+            conf["modules"].append(mod)
+            continue
+
+        # is there a newer version available?
+        if (mod.get("tag") is not None) and (
+            _tag_as_version(
+                _get_repo_latest_tag(ctx, repo, conf["odoo_version"])[0],
+                conf["odoo_version"],
+            )
+            <= _tag_as_version(mod["tag"], conf["odoo_version"])
+        ):
+            click.echo(f"    no newer tag in repo {repo}")
+            conf["modules"].append(mod)
+            continue
+        new_tag, new_tar_url = _get_repo_latest_tag(ctx, repo, conf["odoo_version"])
+        click.echo(f"    updating to {new_tag} from { mod.get('tag', 'no tag set')}")
+        mod["tag"] = new_tag
+        conf["modules"].append(mod)
+
+        _extract_tarball(ctx, new_tar_url, git_path, mod_path)
+
+    # clear the download cache
+    _clear_download_cache()
     # write back to our config
     with open("vogon.yaml", "w", encoding="utf-8") as f:
         f.write(
