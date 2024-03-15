ee9fc98fc8ffecad7e1e1cbd25eaecde4d360bd7
MrOsdev <142299109+MrOsdev@users.noreply.github.com>
2024-03-15 10:14:59 +0000

    feat: neutralize for Odoo < 16

--- hitchhiker/cli/odoo/neutralize.py
+++ hitchhiker/cli/odoo/neutralize.py
@@ -12,7 +12,51 @@ def neutralize_cmd(env: Any) -> None:  # pylint: disable=unused-argument
     """Neutralize Odoo database"""
     dbname = odoo.tools.config["db_name"]
     click.echo(f"neutralizing database: {dbname}")
-    odoo.modules.neutralize.neutralize_database(env.cr)
+    if odoo.release.version_info[0] >= 16:
+        odoo.modules.neutralize.neutralize_database(env.cr)
+    else:
+        click.echo("odoo < 16 detected - neutralizing manually")
+        env.cr.execute(
+            "SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename = 'ir_mail_server');"
+        )
+        if env.cr.fetchall()[0][0]:
+            click.echo("Deleting ir_mail_server records")
+            env.cr.execute("DELETE FROM ir_mail_server;")
+        env.cr.execute(
+            "SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename = 'fetchmail_server');"
+        )
+        if env.cr.fetchall()[0][0]:
+            click.echo("Deleting fetchmail_server records")
+            env.cr.execute("DELETE FROM fetchmail_server;")
+        env.cr.execute(
+            "SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename = 'ir_cron');"
+        )
+        if env.cr.fetchall()[0][0]:
+            click.echo("Disabling ir_cron records")
+            env.cr.execute(
+                "UPDATE ir_cron SET active = false WHERE id NOT IN ("
+                + "SELECT res_id FROM ir_model_data "
+                + "WHERE model = 'ir.cron' "
+                + "AND name = 'autovacuum_job' "
+                + "AND module = 'base');"
+            )
+        env.cr.execute(
+            "SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename = 'account_online_link');"
+        )
+        if env.cr.fetchall()[0][0]:
+            click.echo("Deactivating online synchronisation")
+            env.cr.execute("UPDATE account_online_link SET auto_sync = false;")
+        env.cr.execute(
+            "SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename = 'ir_config_parameter');"
+        )
+        if env.cr.fetchall()[0][0]:
+            click.echo("Setting database.is_neutralized")
+            env.cr.execute(
+                "INSERT INTO ir_config_parameter (key, value) "
+                + "VALUES ('database.is_neutralized', true) "
+                + "ON CONFLICT (key) DO "
+                + "UPDATE SET value = true;"
+            )
     click.echo("neutralized.")
 
 
